0a1761d60e5effaae8c338a05701bfd7eed4e79d
elkolorado <filipek27@poczta.fm>
2019-09-18 11:09:07 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,55 +1,28 @@
 var text = "hello";
 var betweenChars = ' '; // a space
 
-// alert(text.split('').join(betweenChars));
 
 function myFunction(inputElement) {
-  document.getElementById("myText").value = ((document.getElementById("myText").value).split('').join(betweenChars));
-  /* Get the text field */
-var copyText = document.getElementById("myText");
+    document.getElementById("myText").value = ((document.getElementById("myText").value).split('').join(betweenChars));
 
-/* Select the text field */
-copyText.select();
 
-/* Copy the text inside the text field */
-document.execCommand("copy");
-inputElement.setSelectionRange(0, 0);
-inputElement.blur();
+    var copyText = document.getElementById("myText");
+    copyText.select();
+    document.execCommand("copy");
+
 }
 
 
-function toWeirdCase(string) {
-  var reg = /\b(?![\s.])/
-  var res = string.split(reg)
-  var newArr = []
-
-  for (let k = 0; k < res.length; k++) {
-    let newString = "";
-    for (let j = 0; j < res[k].length; j++) {
-      if (j % 2 == 0) {
-        newString += res[k].charAt(j).toUpperCase()
-      } else {
-        newString += res[k].charAt(j).toLowerCase()
-      }
-    }
-    newArr.push(newString)
-  }
-  return newArr.join('')
+function bob(string) {
+    return string.split('').map(function(c, i) {
+        return i & 1 ? c.toUpperCase() : c.toLowerCase();
+    }).join('');
 }
 
 
 function myFunction2(inputElement) {
-  document.getElementById("myText").value = ((document.getElementById("myText").value).toWeirdCase());
-
-
-
-
-var copyText = document.getElementById("myText");
-
-copyText.select();
-
-/* Copy the text inside the text field */
-document.execCommand("copy");
-inputElement.setSelectionRange(0, 0);
-inputElement.blur();
+  document.getElementById("myText").value = bob(document.getElementById("myText").value);
+  var copyText = document.getElementById("myText");
+  copyText.select();
+  document.execCommand("copy");
 }
