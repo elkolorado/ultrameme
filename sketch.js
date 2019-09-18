793b9dc9634b51e3b2a90e91c717c27b425814da
elkolorado <filipek27@poczta.fm>
2019-09-18 09:18:58 +0200

    new generate
    new generate

--- sketch.js
+++ sketch.js
@@ -16,3 +16,28 @@ document.execCommand("copy");
 inputElement.setSelectionRange(0, 0);
 inputElement.blur();
 }
+
+
+String.prototype.toRandomCase = function() {
+    return this.split('').map(function(c){
+        return c[Math.round(Math.random())?'toUpperCase':'toLowerCase']();
+    }).join('');
+}
+
+
+function myFunction2(inputElement) {
+  document.getElementById("myText").value = ((document.getElementById("myText").value).toRandomCase());
+
+
+
+  /* Get the text field */
+var copyText = document.getElementById("myText");
+
+/* Select the text field */
+copyText.select();
+
+/* Copy the text inside the text field */
+document.execCommand("copy");
+inputElement.setSelectionRange(0, 0);
+inputElement.blur();
+}
